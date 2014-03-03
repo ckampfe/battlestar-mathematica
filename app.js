9b9eb1784b0d6900af6dab69db4170ee066dd44e
Clark Kampfe <clark.kampfe@gmail.com>
2014-03-02 21:04:25 -0600

    routes, jade

--- app.js
+++ app.js
@@ -5,11 +5,15 @@ var io      = require('socket.io').listen(server);
 var path    = require('path');
 var math    = require('mathjs')();
 
+var routes  = require(__dirname + '/routes');
 var problem = require(__dirname + '/public/javascripts/helpers/problem')();
 var H       = require(__dirname + '/public/javascripts/helpers/util')();
 
 app.use(express.static(__dirname + '/public'));
+app.set('view engine', 'jade');
 
 require('./sockets')(io, H, problem, math);
+
+app.get('/', routes.index);
 
 server.listen(3000);
